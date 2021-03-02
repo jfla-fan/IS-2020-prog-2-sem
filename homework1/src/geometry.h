2c634b1dcbcf7df31577ef077289ca39f0d407fd
jfla-fan <66958733+jfla-fan@users.noreply.github.com>
2021-03-02 16:12:54 +0300

    Delete homework1/src directory
    changes...

--- homework1/src/geometry.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "public\Point.h"
-#include "public\PolygonalChain.h"
-#include "public\ClosedPolygonalChain.h"
-#include "public\Polygon.h"
-#include "public\RegularPolygon.h"
-#include "public\Trapezoid.h"
-#include "public\Triangle.h"
